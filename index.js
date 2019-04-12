ce56484fa3f0f7b4a1f1c548df01f3011b83caf3
danthevogt <danielharlowvogt@gmail.com>
2019-04-12 14:48:02 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+//gives access to the express library
+let express = require("express");
+
+//creates an express application obkexct
+let app = express();
+
+//gives access to the path
+//port communication
+let path = require("path");
+
+//extracting data from the request
+let port = 3000;
+
+//gives acess to the body-parser library used in the req extracting
+//data from the request IOW the form
+let bodyParser = require("body-parser");
+
+//gives access to knex which provides SQL in out app
+let knex = require("knex")({
+    client: 'sqlite3',
+    connection: {
+        filename: "./MusicLibrary.db"
+    },
+    useNullAsDefault: true
+})
+
+//allows us to use objects and arrays like JSON format
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//this specifies how our EJS pages are converted to HTML
+//and helps the browser so it can display it
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    knex.select('SongID', 'SongName', 'ArtistID', 'YearReleased').from('Songs').orderBy('SongID').then(Songs =>{
+        res.render('index', {test: Songs});
+      }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname + "/about.html"));
+});
+
+app.get("/email", function(req, res) {
+    res.sendFile(path.join(__dirname + "/emailpage.html"));
+});
+
+app.listen(port, function() {
+    console.log("I am now listening");
+});
